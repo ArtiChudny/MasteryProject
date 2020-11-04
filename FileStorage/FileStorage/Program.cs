e09b50a6f413fb4c41f554ab0ee80b61aab9b47e
ArtiChudny <chudnyi3@mail.ru>
2020-11-04 15:19:43 +0300

    [lab02] - add authorization to the app

--- FileStorage/FileStorage/Program.cs
+++ FileStorage/FileStorage/Program.cs
@@ -6,7 +6,14 @@ namespace FileStorage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (StorageCommand.LoginToApp(args))
+            {
+                Console.WriteLine("You logged in.");
+            }
+            else
+            {
+                Environment.Exit(-1);
+            }
         }
     }
 }
---
+++ FileStorage/FileStorage/StorageCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using FileStorage.Models;
+
+static class StorageCommand
+{
+    public static bool LoginToApp(string[] args)
+    {
+        if (args.Count() == 4 && (args[0] == "--l" || args[2] == "--l") && (args[0] == "--p" || args[2] == "--p"))
+        {
+            if (args[0] == "--l" && args[2] == "--p")
+            {
+                return CheckAuthentification(args[1], args[3]);
+            }
+            else
+            {
+                return CheckAuthentification(args[3], args[1]);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Uncorrect initial parameters. Use --l for login and --p for password.");
+            return false;
+        }
+    }
+
+    public static bool CheckAuthentification(string login, string password)
+    {
+        if (login == StorageInfo.login && password == StorageInfo.password)
+        {
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect login or password");
+            return false;
+        }
+    }
+}
