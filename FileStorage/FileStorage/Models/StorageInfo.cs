4ba2495ee0eb9a9d76b6c915a8c1c7f701803e07
ArtiChudny <chudnyi3@mail.ru>
2020-11-04 11:30:01 +0300

    [lab02] - add class StorageInfo and package System.Configuration.ConfigurationManager

---
+++ FileStorage/FileStorage/Models/StorageInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+
+namespace FileStorage.Models
+{
+    static class StorageInfo
+    {
+        public static readonly string login;
+        public static readonly string password;
+        public static readonly string maxStorage;
+        public static readonly string creationDate;
+
+        static StorageInfo()
+        {
+            login = ConfigurationManager.AppSettings["login"];
+            password = ConfigurationManager.AppSettings["password"];
+            maxStorage = ConfigurationManager.AppSettings["maxStorage"];
+            creationDate = ConfigurationManager.AppSettings["creationDate"];
+        }
+    }
+}
