f008f6356a2a4f83189ded6119f0caed6d53a2fd
ArtiChudny <chudnyi3@mail.ru>
2020-11-09 12:34:18 +0300

    [lab02] - add StorageFile model, and change StorageInfo model

---
+++ FileStorage/FileStorage/Models/StorageFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileStorage.Models
+{
+    [Serializable]
+    public class StorageFile
+    {
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+        public int Size { get; set; }
+        public int DownloadsNumber { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
--- FileStorage/FileStorage/Models/StorageInfo.cs
+++ FileStorage/FileStorage/Models/StorageInfo.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FileStorage.Models
 {
+    [Serializable]
     public class StorageInfo
     {
+        public int MaxStorage { get; set; }
         public int UsedStorage { get; set; }
         public DateTime CreationDate { get; set; }
+        public List<StorageFile> StorageFiles { get; set; }
+
+        public StorageInfo()
+        {
+            MaxStorage = 536870912; //512MB int bytes
+            UsedStorage = 0;
+            CreationDate = DateTime.Today;
+            StorageFiles = new List<StorageFile>();
+        }
     }
 }
