b872562d74cfbec749f9eebd6f792b2a64fa9ef7
ArtiChudny <chudnyi3@mail.ru>
2020-11-11 17:17:32 +0300

    [lab02] - remove properties 'MaxFileSize' and 'MaxStorage' from storageInfo model

--- FileStorage/FileStorage/Models/StorageInfo.cs
+++ FileStorage/FileStorage/Models/StorageInfo.cs
@@ -6,19 +6,15 @@ namespace FileStorage.Models
     [Serializable]
     public class StorageInfo
     {
-        public long MaxStorage { get; set; }
-        public long MaxFileSize { get; set; }
         public long UsedStorage { get; set; }
         public DateTime CreationDate { get; set; }
-        public List<StorageFile> StorageFiles { get; set; }
+        public IList<StorageFile> Files { get; set; }
 
         public StorageInfo()
         {
-            MaxStorage = 5368709120; //5GB
-            MaxFileSize = 2147483648; //2GB
             UsedStorage = 0;
             CreationDate = DateTime.Today;
-            StorageFiles = new List<StorageFile>();
+            Files = new List<StorageFile>();
         }
     }
 }
